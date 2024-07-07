87f2f213bbae7a035eae748e80c92700a5bb910a
mtharreqq <mtharreq@gmail.com>
2024-07-07 16:45:48 +0700

    fix: responsive design for landing page in mobile and web view

--- src/views/LatestArticles.js
+++ src/views/LatestArticles.js
@@ -1,68 +1,99 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from "react-redux"
-import { getArticleList } from "@store/api/article"
-import { Link } from 'react-router-dom'
+/* eslint-disable semi */
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getArticleList } from "@store/api/article";
+import { Link } from "react-router-dom";
 
-const ArticleTabs = ({ activeTab, setActiveTab }) => (
-  <div className="tabs">
-    {['All Items', 'News', 'Announcements', 'Tutorials'].map(tab => (
-      <button key={tab} className={`tab ${activeTab === tab ? 'active' : ''}`} onClick={() => setActiveTab(tab)}>
-        {tab}
-      </button>
-    ))}
-  </div>
-)
+const tabs = ["All Items", "News", "Announcements", "Tutorials"];
 
-const ArticleCard = ({ article }) => (
-  <div className="article-card">
-    <img src={article.image} alt="Article Thumbnail" className="article-image" />
-    <div className="article-content">
-      <div className="tag-wrapper">
-        <span className={`tag ${article.category.replace(/\s/g, '')}`}>{article.type}</span>
-      </div>
-      <h3 className="article-title">{article.title}</h3>
-      <p className="article-summary">{article.summary}</p>
-      <div className="article-footer">
-        <div className="author-info">
-          <img src="https://via.placeholder.com/50" alt="Author" className="author-image" />
-          <div>
-            <span className="author-name">{article.author}</span>
-            <span className="author-date">{article.date}</span>
-          </div>
-        </div>
-        <Link to={`/article/${article.id}`} className="read-more-link">Read More</Link>
-      </div>
-    </div>
-  </div>
-)
-
-const LatestArticles = () => {
-  const dispatch = useDispatch()
-  const [activeTab, setActiveTab] = useState('All Items')
-  // const articles = useSelector(state => state.articles.articles);  // Use articles from Redux
-  const { articles, isLoading } = useSelector((state) => state.article)
-  const filteredArticles = articles.filter(article => activeTab === 'All Items' || article.type === activeTab)
-
-  useEffect(() => {
-    dispatch(getArticleList())
-    // console.log(articles);
-  }, [])
-
+function ArticleTabs({ activeTab, setActiveTab }) {
   return (
-    <section className="blog-section">
-
-       <div className="container">
-        <h1 className="heading1x">Latest Articles</h1>
-        <p>Discover the most outstanding AI Articles</p>
-        <ArticleTabs activeTab={activeTab} setActiveTab={setActiveTab} />
-        <div className="articles-grid">
-          {filteredArticles.map(article => (
-            <ArticleCard key={article.id} article={article} />
-          ))}
-        </div>
-      </div>
-    </section>
-  )
+    <div className="tabs">
+      {tabs.map((tab) => (
+        <button
+          key={tab}
+          className={`tab ${activeTab === tab ? "active" : ""}`}
+          onClick={() => setActiveTab(tab)}
+        >
+          {tab}
+        </button>
+      ))}
+    </div>
+  );
 }
 
-export default LatestArticles
+function formatDate(isoDateString) {
+  const date = new Date(isoDateString);
+  const day = date.getDate();
+  const month = date.toLocaleString("default", { month: "long" });
+  const year = date.getFullYear();
+
+  return `${day} ${month} ${year}`;
+}
+
+function ArticleCard({ article }) {
+  return (
+    <div className="article-card">
+      <figure className="article-image">
+        <img src={article.image} alt="Article Thumbnail" />
+      </figure>
+      <div className="article-content">
+        <div className={`article-tag ${article.category.replace(/\s/g, "")}`}>
+          {article.category}
+        </div>
+
+        <h3 className="article-title">{article.title}</h3>
+        <p className="article-summary">{article.summary}</p>
+        <div className="article-footer">
+          <div className="author">
+            <img src={article.authorImage} alt="Author" />
+            <div className="author-info">
+              <p className="author-name">{article.author}</p>
+              <p className="author-date">{formatDate(article.date)}</p>
+            </div>
+          </div>
+          <Link to={`/article/${article.id}`} className="read-more">
+            Read More
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function LatestArticles() {
+  const dispatch = useDispatch();
+  const [activeTab, setActiveTab] = useState("All Items");
+  const { articles } = useSelector((state) => state.article);
+  const filteredArticles = articles.filter(
+    (article) => activeTab === "All Items" || article.type === activeTab
+  );
+
+  useEffect(() => {
+    dispatch(getArticleList());
+    console.log(articles);
+  }, []);
+
+  return (
+    <section className="latest-articles">
+      <h1>Latest Articles</h1>
+      <p>Discover the most outstanding AI Articles</p>
+      <ArticleTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+      <div className="articles-grid">
+        {filteredArticles.map((article) => (
+          <>
+            <ArticleCard key={article.id} article={article} />
+            {/* TODO: DUMMY ARTICLE FOR DEV */}
+            <ArticleCard key={2} article={article} />
+            <ArticleCard key={3} article={article} />
+            <ArticleCard key={4} article={article} />
+            <ArticleCard key={5} article={article} />
+            <ArticleCard key={6} article={article} />
+          </>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default LatestArticles;
